b552b3f6c3c4b44d528e4d2ee70ba8f1c1cc3d8d
Eric Marrone <emarrone17@gmail.com>
2017-11-28 17:01:24 -0500

    Create partial view for review form. Add edit page and review edit/update actions in controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -15,4 +15,25 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:product_id])
+    product = Product.find(params[:product_id])
+    @review = Review.find(params[:id])
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      flash[:notice] = "You have successfully edited the review."
+      redirect_to product_url(product)
+    else
+      render '/reviews/edit'
+    end
+
+  end
+
 end
