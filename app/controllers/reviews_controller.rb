0c097559c5721c67c5ed1d4e9379551f08960354
Lena <fatmeow@gmail.com>
2017-11-28 16:41:33 -0500

    Add redirection and render path along with review and product global variable

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,18 @@
 class ReviewsController < ApplicationController
   def create
+    @product = Product.find(params[:product_id])
+    product = Product.find(params[:product_id])
 
+    @review = Review.new
+    @review.comment = params[:review][:comment]
+    @review.product_id = params[:product_id]
+
+    if @review.save
+      flash[:notice] = "You have successfully saved the review."
+      redirect_to product_url(product)
+    else
+      render '/products/show'
+    end
   end
 
 end
