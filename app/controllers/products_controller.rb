980c7eaac5cc7e9686d549818de79829b06fca4d
Lena <fatmeow@gmail.com>
2017-11-28 12:41:50 -0500

    Add redirection to show product page if form is saved, and render new page if not

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,7 @@ class ProductsController < ApplicationController
     if @product.save
       redirect_to product_path(@product)
     else
-      redirect_to new_product_path
+      render :new
     end
   end
 
