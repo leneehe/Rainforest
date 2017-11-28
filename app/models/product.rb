cdf64760c7c66869c5cb9d3a82dee8b5633b69c6
Eric Marrone <emarrone17@gmail.com>
2017-11-28 14:52:37 -0500

    Add review and product model associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :reviews
   validates :name, :description, :price, presence:  true
   validates :price, numericality: {only_integer: true, greater_than: 0}
 
