80b79721702136ddb3ca184cef068ec816b64e80
Lena <fatmeow@gmail.com>
2017-11-29 22:04:33 -0500

    Add user model for sign up, add session controller and view for login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128195352) do
+ActiveRecord::Schema.define(version: 20171130011345) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20171128195352) do
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
     t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
